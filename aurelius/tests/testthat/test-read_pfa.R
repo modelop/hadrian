d7949111d44919e709b110b32e6e6fbf719aba07
Steven Mortimer <reportmort@gmail.com>
2017-01-13 23:59:48 -0500

    Change Read and Write Methods
    Change the methods for reading and writing PFA so that the function names are more obvious, handle more input and output types, and leverages jsonlite package to decrease the size of written files by over 10% because of decreased whitespace

---
+++ aurelius/tests/testthat/test-read_pfa.R
@@ -0,0 +1,19 @@
+context("read_pfa")
+
+test_that("read_xml works with all input object types", {
+
+  model_as_list <- list(input='double', output='double', action=list(list(`+`=list('input', 10))))
+  
+  # literal JSON string  (useful for small examples)
+  toy_model <- read_pfa('{"input": "double", "output": "double", "action": [{"+": ["input", 10]}]}')
+  expect_identical(toy_model, model_as_list)
+  
+  # from a local path
+  local_model <- read_pfa(system.file("extdata", "my-model.pfa", package = "aurelius"))
+  expect_identical(local_model, model_as_list)
+  
+  # from a url
+  url_model <- read_pfa("https://raw.githubusercontent.com/ReportMort/hadrian/master/aurelius/inst/extdata/my-model.pfa")
+  expect_identical(url_model, model_as_list)
+  
+})
