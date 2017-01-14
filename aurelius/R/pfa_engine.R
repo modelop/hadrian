105634d1d1fdd2257a290cb138c3be47caddc664
Steven Mortimer <reportmort@gmail.com>
2017-01-14 00:04:10 -0500

    Add pfa underscore prefixed functions

---
+++ aurelius/R/pfa_engine.R
@@ -0,0 +1,85 @@
+# Copyright (C) 2014  Open Data ("Open Data" refers to
+# one or more of the following companies: Open Data Partners LLC,
+# Open Data Research LLC, or Open Data Capital LLC.)
+# 
+# This file is part of Hadrian.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+counter <- new.env()
+counter$n <- 0
+
+#' pfa_engine
+#'
+#' Create an executable PFA scoring engine in R by calling Titus through rPython. 
+#' If this function is successful, then the PFA is valid (only way to check 
+#' PFA validity in R).
+#' 
+#' @param config \code{list} of lists representing a complete PFA document
+#' @param temp_file if NULL, generate the PFA as a string in memory and pass it 
+#' without touching disk; if a string, save the PFA document in a temporary file 
+#' and have Python load it from that file
+#' @return an object with a $action(...) method that can be used to score data
+#' @importFrom rPython python.exec python.call
+#' @source json.R
+#' @export pfa_engine
+#' @examples
+#' my_pfa_doc <- pfa_document(avro_double, avro_double, expression(input + 10))
+#' pfa_engine(my_pfa_doc)   # where the my_pfa_doc is created by pfa_document
+
+pfa_engine <- function(config, temp_file = NULL) {
+  
+    counter$n <- counter$n + 1
+    name <- paste("engine_", counter$n, sep = "")
+    method <- config$method
+
+    # load PFA into Titus and get an error message if it's malformed
+    python.exec("import json")
+    python.exec("from titus.genpy import PFAEngine")
+    python.exec("def do(*args): return args[-1]")
+
+    if (is.null(temp_file)) {
+        python.exec(paste(name, " = PFAEngine.fromJson(json.loads(r\"\"\"", read_pfa(config), "\"\"\"))[0]", sep=""))
+    }
+    else {
+        write_pfa(config, file = temp_file)
+        python.exec(paste(name, " = PFAEngine.fromJson(json.load(open(\"", temp_file, "\")))[0]", sep=""))
+    }
+
+    # work-around because python.method.call doesn't seem to work for my auto-generated stuff
+    python.exec(paste(name, "_begin = lambda: ", name, ".begin()", sep = ""))
+    
+    if (!is.null(method)  &&  method == "emit") {
+        python.exec(paste(name, "_emitted = []", sep = ""))
+        python.exec(paste(name, ".emit = lambda x: ", name, "_emitted.append(x)", sep = ""))
+        python.exec(paste(name, "_action = lambda x: do(", name, ".action(x), ", name, "_emitted.reverse(), [", name, "_emitted.pop() for i in xrange(len(", name, "_emitted))])", sep = ""))
+    }
+    else {
+      python.exec(paste(name, "_action = lambda x: ", name, ".action(x)", sep = ""))
+    }
+    
+    python.exec(paste(name, "_end = lambda: ", name, ".end()", sep = ""))
+    python.exec(paste(name, "_actionsStarted = lambda: ", name, ".actionsStarted", sep = ""))
+    python.exec(paste(name, "_actionsFinished = lambda: ", name, ".actionsFinished", sep = ""))
+    python.exec(paste(name, "_config = lambda: ", name, ".config.jsonNode(False, set())", sep = ""))
+    python.exec(paste(name, "_snapshot = lambda: ", name, ".snapshot().jsonNode(False, set())", sep = ""))
+
+    # provide a thing like a class
+    list(begin = function() python.call(paste(name, "_begin", sep="")),
+         action = function(input) python.call(paste(name, "_action", sep=""), input, simplify = FALSE),
+         end = function() python.call(paste(name, "_end", sep="")),
+         actionsStarted = function() python.call(paste(name, "_actionsStarted", sep="")),
+         actionsFinished = function() python.call(paste(name, "_actionsFinished", sep="")),
+         config = function() python.call(paste(name, "_config", sep=""), simplify = FALSE),
+         snapshot = function() python.call(paste(name, "_snapshot", sep=""), simplify = FALSE))
+}
+
